e4cb48f4fd21de34b84ea155c14353d07b032fdb
Sergio Campos <morrodogg8@gmail.com>
2019-07-12 17:44:27 -0700

    finished

--- e3.15/main.cpp
+++ e3.15/main.cpp
@@ -1,15 +1,32 @@
-//
-//  main.cpp
-//  e3.15
-//
-//  Created by Sergio campos on 7/12/19.
-//  Copyright © 2019 Sergio campos. All rights reserved.
-//
+/*
+ •• E3.15
+ Write a program that reads in two floating-point numbers and tests whether they are the same up to two decimal places. Here are two sample runs.
+ 
+ Enter two floating-point numbers: 2.0 1.99998
+ They are the same up to two decimal places.
+ Enter two floating-point numbers: 2.0 1.98999
+ They are different.
+ */
 
 #include <iostream>
+#include <cmath>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+int main()
+{
+    cout << "Enter two floating-point numbers: ";
+    float first, second;
+    cin >> first >> second;
+    
+    cout << abs(first - second) << endl;
+    if (abs(first - second) <= 0.001)
+    {
+        cout << "They are the same up two decimal places.";
+    }
+    else
+    {
+        cout << "They are different";
+    }
+    cout << endl;
 }
